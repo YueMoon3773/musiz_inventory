5eb4cf4d67c61669f65e36d3f0a28695fd5c1902
YueMoon3773 <giabaonguynhas@gmail.com>
2026-01-31 23:13:42 +0700

    update pool for Aiven CA requirement

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,14 +1,13 @@
-const fs = require('fs');
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const ssl = process.env.DB_CA
+    ? { require: true, rejectUnauthorized: true, ca: process.env.DB_CA }
+    : { require: true, rejectUnauthorized: false };
+
 const pool = new Pool({
     connectionString: `${process.env.DB_URL}`,
-    ssl: {
-        require: true,
-        rejectUnauthorized: true,
-        ca: fs.readFileSync('./ca.pem').toString(),
-    },
+    ssl,
     keepAlive: true,
     max: 20,
     idleTimeoutMillis: 30000,
